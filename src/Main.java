45cbef6adae76fe14a361d2a62f335a223f3270d
WhyRoX <enes.12.yumer@gmail.com>
2025-03-13 11:05:43 +0100

    add main project files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph("artists.txt", "mentions.txt");
+        graph.trouverCheminLePlusCourt("The Beatles", "Kendji Girac");
+		System.out.println("--------------------------");
+
+        graph.trouverCheminMaxMentions("The Beatles", "Kendji Girac");
+    }
 }
