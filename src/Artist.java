5aed0488018dc6841de3302d37142398c3e93cd2
Alexandre Taillet <altata01@hotmail.com>
2025-03-13 12:34:25 +0100

    refactor: update Artist class to support multiple categories and improve Graph file reading

--- src/Artist.java
+++ src/Artist.java
@@ -1,15 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Artist {
 
   private final int id;
   private final String nom;
   private final String[] categories;
+  private final Map<Artist, Integer> poids;
+  private final Set<Artist> artisteAdjacent = new HashSet<>();
 
   public Artist(int id, String nom, String... categories) {
     this.id = id;
     this.nom = nom;
     this.categories = categories;
+    this.poids = new HashMap<>();
   }
 
 
@@ -25,12 +28,34 @@ public class Artist {
     return categories;
   }
 
+  public Map<Artist, Integer> getPoids() {
+    return poids;
+  }
+
+  public Set<Artist> getArtisteAdjacent() {
+    return artisteAdjacent;
+  }
+
   @Override
   public String toString() {
     return "Artist{" +
             "id=" + id +
             ", nom='" + nom + '\'' +
-            ", categories='" + Arrays.toString(categories) + '\'' +
+            ", categories=" + Arrays.toString(categories) +
+            ", poids=" + poids.entrySet().stream().map(e -> e.getKey().getId()+":"+e.getValue()).toList() +
+            ", artisteAdjacent=" + artisteAdjacent +
             '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Artist artist = (Artist) o;
+    return id == artist.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
   }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,13 +1,11 @@
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 import java.util.function.Consumer;
 
 public class Graph {
-    Map<Integer, Artist> artists = new HashMap<>();
+    Map<Integer, Artist> artistes = new HashMap<>();
     public Graph(String artistsFilePath, String mentionsFilePath) {
         File artistFile = new File(artistsFilePath);
         File mentionFile = new File(mentionsFilePath);
@@ -15,18 +13,25 @@ public class Graph {
             String[] split = line.split(",");
             String[] categories = split[2].split(";");
             Artist artist = new Artist(Integer.parseInt(split[0]),split[1],categories);
-            artists.put(artist.getId(), artist);
+            artistes.put(artist.getId(), artist);
             System.out.println(artist);
         });
 
         readLineByLine(mentionFile,string -> {
             String[] split = string.split(",");
-            Artist artist1 = artists.get(Integer.parseInt(split[0]));
-            Artist artist2 = artists.get(Integer.parseInt(split[1]));
+            Artist artist1 = artistes.get(Integer.parseInt(split[0]));
+            Artist artist2 = artistes.get(Integer.parseInt(split[1]));
             int mentions = Integer.parseInt(split[2]);
-
-            //System.out.println(artist1.getNom() + "est en lien avec " + artist2.getNom() +" avec "+ mentions+" mentions");
+            artist1.getPoids().put(artist2,mentions);
         });
+
+        int i = 0;
+        for (Artist a : artistes.values()){
+            System.out.println("for logic");
+            if(i++ >= 1000)
+                break;
+            System.out.println(a);
+        }
     }
 
     private void readLineByLine(File file, Consumer<String> consumer){
@@ -42,8 +47,15 @@ public class Graph {
         }
     }
 
-    public void trouverCheminLePlusCourt(String string, String string2) {
+    public void trouverCheminLePlusCourt(String nomArtiste1, String nomArtiste2) {
         // TODO Auto-generated method stub
+        if(!artistes.containsKey(nomArtiste1) || !artistes.containsKey(nomArtiste2)){
+            throw new IllegalArgumentException("Les arguments ne sont pas valides");
+        }
+
+        Artist artist1 = artistes.get(nomArtiste1);
+        Artist artist2 = artistes.get(nomArtiste2);
+
         throw new UnsupportedOperationException("Unimplemented method 'trouverCheminLePlusCourt'");
     }
 
